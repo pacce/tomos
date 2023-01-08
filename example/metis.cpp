47d5acaebb649f4b745d105a176a9a7b0bb1fadd
Pedro Henrique Dalla Pacce <pedrodallapacce@gmail.com>
2023-01-07 22:27:50 -0300

    feat: builds dual graph from mesh

--- example/metis.cpp
+++ example/metis.cpp
@@ -1,6 +1,25 @@
+#include <cstdlib>
+#include <filesystem>
+#include <iostream>
+#include <mesh/mesh.hpp>
 #include <tomos/tomos.hpp>
 
 int
 main(int argc, char** argv) {
-    return 0;
+    try {
+        if (argc != 2) { throw std::invalid_argument("invali number of arguments"); }
+        mesh::Mesh<float> mesh = mesh::decode<float>(std::filesystem::path{argv[1]});
+        auto values = tomos::metis::dual(mesh);
+
+        for (const auto& [key, vs] : values) {
+            std::cout << key << " : ";
+            for (const auto& v : vs) {
+                std::cout << v << " ";
+            }
+            std::cout << std::endl;
+        }
+    } catch (const std::exception& e) {
+        exit(EXIT_FAILURE);
+    }
+    exit(EXIT_SUCCESS);
 }
--- include/tomos/tomos-metis.hpp
+++ include/tomos/tomos-metis.hpp
@@ -1,14 +1,59 @@
 #ifndef TOMOS_METIS_HPP__
 #define TOMOS_METIS_HPP__
 
+#include <iostream>
 #include <mesh/mesh.hpp>
 #include <metis.h>
 
 namespace tomos {
 namespace metis {
     template <typename Precision>
-    void
-    dual(const mesh::Mesh<Precision>& msh) {
+    std::map<mesh::element::Number, std::vector<mesh::element::Number>>
+    dual(const mesh::Mesh<Precision>& mesh) {
+        idx_t ne = static_cast<idx_t>(mesh.element.size());
+        idx_t nn = static_cast<idx_t>(mesh.nodes.size());
+
+        std::vector<idx_t> eptr = {0};
+        std::vector<idx_t> eind = {};
+
+        std::map<mesh::element::Number, std::vector<mesh::element::Number>> values;
+
+        for (const auto& [key, e] : mesh.element) {
+            values[key] = {};
+
+            eptr.push_back(e.nodes.size() + eptr.back());
+            for (const mesh::node::Number& n : e.nodes) { eind.push_back(n - 1); }  // C-style 0-based indexing
+        }
+
+        idx_t numflag = 0;  // C-style 0-based indexing
+        idx_t ncommon = 2;  // 2 common nodes per edge
+
+        idx_t * xadj;
+        idx_t * adjncy;
+
+        METIS_MeshToDual(
+                  &ne   // number of elements
+                , &nn   // number of nodes
+                , eptr.data()
+                , eind.data()
+                , &ncommon
+                , &numflag
+                , &xadj
+                , &adjncy
+                );
+
+        idx_t * prev = xadj;
+        idx_t * next = xadj + 1;
+        for (auto& [key, value] : values) {
+            for (idx_t j = *prev; j < *next; j++) { value.push_back(adjncy[j] + 1); }
+
+            prev++; next++;
+        }
+
+        METIS_Free(xadj);
+        METIS_Free(adjncy);
+
+        return values;
     }
 } // namespace metis
 } // namespace tomos
