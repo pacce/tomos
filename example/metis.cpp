47e9fbcdb5d5d7fbe1cd26a4d7ed81d4c308c588
Pedro Henrique Dalla Pacce <pedrodallapacce@gmail.com>
2023-01-08 10:57:48 -0300

    feat: adds nodal mesh adjacency function

--- example/metis.cpp
+++ example/metis.cpp
@@ -8,10 +8,20 @@ int
 main(int argc, char** argv) {
     try {
         if (argc != 2) { throw std::invalid_argument("invali number of arguments"); }
-        mesh::Mesh<float> mesh = mesh::decode<float>(std::filesystem::path{argv[1]});
-        auto values = tomos::metis::dual(mesh, tomos::metis::Common::EDGE);
+        mesh::Mesh<float> mesh          = mesh::decode<float>(std::filesystem::path{argv[1]});
+        tomos::metis::Adjacency dual    = tomos::metis::dual(mesh, tomos::metis::Common::EDGE);
 
-        for (const auto& [key, vs] : values) {
+        for (const auto& [key, vs] : dual) {
+            std::cout << key << " : ";
+            for (const auto& v : vs) {
+                std::cout << v << " ";
+            }
+            std::cout << std::endl;
+        }
+
+        std::cout << std::endl;
+        tomos::metis::Adjacency nodal = tomos::metis::nodal(mesh);
+        for (const auto& [key, vs] : nodal) {
             std::cout << key << " : ";
             for (const auto& v : vs) {
                 std::cout << v << " ";
