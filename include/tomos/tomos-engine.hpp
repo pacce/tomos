2aea1151e872f93b7e8ab5d2609e3d33c4ebd3e8
Pedro Henrique Dalla Pacce <pedrodallapacce@gmail.com>
2023-01-22 13:30:01 -0300

    refactor: simplifies engine device selection

--- include/tomos/tomos-engine.hpp
+++ include/tomos/tomos-engine.hpp
@@ -8,45 +8,17 @@
 #include <mesh/mesh.hpp>
 
 namespace tomos {
-namespace engine {
-namespace host {
-    template <typename Precision>
-    std::vector<cl_float3>
-    nodes(const mesh::Mesh<Precision>& mesh) {
-        std::vector<cl_float3> xs(mesh.nodes.size());
-
-        std::size_t index = 0;
-        for (const auto& [_, node] : mesh.nodes) {
-            xs[index++] = {node.x(), node.y(), node.z()};
-        }
-        return xs;
-    }
-} // namespace host
-namespace device {
-    cl::Device
-    choose(cl_device_type model);
-
-    template <typename Precision>
-    cl::Buffer
-    nodes(cl::Context& context, const mesh::Mesh<Precision>& mesh) {
-        std::vector<cl_float3> xs = host::nodes(mesh);
-        return {context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, xs.size() * sizeof(cl_float3), xs.data()};
-    }
-} // namespace device
-    std::string
-    kernel(const std::filesystem::path& path);
-} // namespace engine
     class Engine {
         public:
             Engine(cl_device_type model)
-                : device_(engine::device::choose(model))
+                : device_(Engine::device(model))
                 , context_(device_)
             {}
 
             template <typename Precision>
             std::vector<float>
             area(const mesh::Mesh<Precision>& mesh, const std::filesystem::path& path) {
-                std::string source = engine::kernel(path);
+                std::string source = Engine::kernel(path);
 
                 cl::CommandQueue queue(context_, device_);
                 cl::Program program(context_, cl::Program::Sources({source}));
@@ -58,7 +30,7 @@ namespace device {
                     for (const mesh::node::Number& node : element.nodes) { indices.push_back(node - 1); }
                 }
 
-                cl::Buffer nodes    = engine::device::nodes(context_, mesh);
+                cl::Buffer nodes    = this->nodes(mesh);
                 cl::Buffer elements = this->buffer(indices, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR);
                 cl::Buffer values   = this->buffer<float>(mesh.element.size(), CL_MEM_READ_WRITE);
 
@@ -76,6 +48,41 @@ namespace device {
                 return xs;
             }
         private:
+            static cl::Device
+            device(cl_device_type model) {
+                std::vector<cl::Platform> platforms;
+                cl::Platform::get(&platforms);
+
+                if (platforms.empty()) {
+                    throw std::runtime_error("could not find a valid OpenCL platform");
+                }
+
+                for (const cl::Platform& platform : platforms) {
+                    std::vector<cl::Device> devices;
+                    platform.getDevices(model, &devices);
+
+                    for (const cl::Device& device : devices) {
+                        if (not device.getInfo<CL_DEVICE_AVAILABLE>()) { continue; }
+                        return device;
+                    }
+                }
+                throw std::runtime_error("could not find a valid OpenCL device");
+                return {};
+            }
+
+            static std::string
+            kernel(const std::filesystem::path& path) {
+                std::ifstream handle(path);
+                if (not handle.is_open()) {
+                    throw std::runtime_error("could not load OpenCL kernel");
+                }
+
+                std::stringstream ss;
+                ss << handle.rdbuf();
+
+                return ss.str();
+            }
+
             template <typename T>
             cl::Buffer
             buffer(std::vector<T>& vs, cl_mem_flags flag) {
@@ -87,6 +94,19 @@ namespace device {
             buffer(std::size_t size, cl_mem_flags flag) {
                 return {context_, flag, size * sizeof(T)};
             }
+
+            template <typename Precision>
+            cl::Buffer
+            nodes(const mesh::Mesh<Precision>& mesh) {
+                std::vector<cl_float3> xs(mesh.nodes.size());
+
+                std::size_t index = 0;
+                for (const auto& [_, node] : mesh.nodes) {
+                    xs[index++] = {node.x(), node.y(), node.z()};
+                }
+                return buffer(xs, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR);
+            }
+
             cl::Device  device_;
             cl::Context context_;
     };
