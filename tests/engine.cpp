b8c276d46199aad0dd4dca2695d993cfaf7c1442
Pedro Henrique Dalla Pacce <pedrodallapacce@gmail.com>
2023-01-24 12:14:29 -0300

    feat: adds 2 element square mesh stiffness test

--- tests/engine.cpp
+++ tests/engine.cpp
@@ -62,6 +62,47 @@ TEST(Stiffness, Oracle) {
     }
 }
 
+TEST(Stiffness, Square) {
+    const mesh::Mesh<float> mesh = {
+        {2.2, 0, 8}
+        , mesh::node::Map<float>{
+              {1, mesh::Node<float>(0.0, 0.0, 0.0)}
+            , {2, mesh::Node<float>(1.0, 0.0, 0.0)}
+            , {3, mesh::Node<float>(1.0, 1.0, 0.0)}
+            , {4, mesh::Node<float>(0.0, 1.0, 0.0)}
+        }
+        , mesh::element::Map{
+              {1, {mesh::element::Type::TRIANGLE3, {1}, {1, 2, 3}}}
+            , {2, {mesh::element::Type::TRIANGLE3, {1}, {1, 3, 4}}}
+        }
+    };
+    tomos::Engine engine(CL_DEVICE_TYPE_GPU);
+    std::vector<float> expected = {
+           1.0  // (1, 1) -  0
+        , -0.5  // (1, 2) -  1
+        ,  0.0  // (1, 3) -  2
+        , -0.5  // (1, 4) -  3
+        ,  1.0  // (2, 2) -  4
+        , -0.5  // (2, 1) -  5
+        , -0.5  // (2, 3) -  6
+        ,  1.0  // (3, 3) -  7
+        ,  0.0  // (3, 1) -  8
+        , -0.5  // (3, 2) -  9
+        , -0.5  // (3, 4) - 10
+        ,  1.0  // (4, 4) - 11
+        , -0.5  // (4, 1) - 12
+        , -0.5  // (4, 3) - 13
+    };
+    std::vector<float> actual   = engine.color(mesh, "./shaders/tomos.kernel");
+    ASSERT_EQ(actual.size(), expected.size());
+
+    auto kv = tomos::sparse::csr(mesh);
+    for (std::size_t i = 0; i < actual.size(); i++) {
+        EXPECT_FLOAT_EQ(actual[i], expected[i]);
+    }
+}
+
+
 int
 main(int argc, char** argv) {
     testing::InitGoogleTest(&argc, argv);
