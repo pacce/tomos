8fb1532bbfd8d661adada2114793c5d5887b8b40
Pedro Henrique Dalla Pacce <pedrodallapacce@gmail.com>
2023-02-05 12:53:24 -0300

    feat: adds triangle normal estimate

--- tests/engine.cpp
+++ tests/engine.cpp
@@ -59,6 +59,37 @@ TEST(GPU, Centroid) {
     for (std::size_t i = 0; i < actual.size(); i++) { EXPECT_EQ(actual[i], expected[i]); }
 }
 
+TEST(GPU, Normal) {
+    const mesh::Mesh<float> mesh = {
+        {2.2, 0, 8}
+        , mesh::node::Map<float>{
+              {1, mesh::Node<float>(0.0f, 0.0f, 0.0f)}
+            , {2, mesh::Node<float>(1.0f, 0.0f, 0.0f)}
+            , {3, mesh::Node<float>(1.0f, 1.0f, 0.0f)}
+            , {4, mesh::Node<float>(2.0f, 0.0f, 0.0f)}
+            , {5, mesh::Node<float>(2.0f, 2.0f, 0.0f)}
+            , {6, mesh::Node<float>(3.0f, 0.0f, 0.0f)}
+            , {7, mesh::Node<float>(3.0f, 3.0f, 0.0f)}
+        }
+        , mesh::element::Map{
+              {1, {mesh::element::Type::TRIANGLE3, {1}, {1, 2, 3}}}
+            , {2, {mesh::element::Type::TRIANGLE3, {1}, {1, 4, 5}}}
+            , {3, {mesh::element::Type::TRIANGLE3, {1}, {1, 6, 7}}}
+        }
+    };
+    tomos::Engine engine(CL_DEVICE_TYPE_GPU);
+    std::vector<mesh::Node<float>> expected = {
+          {0.0f, 0.0f, 1.0f}
+        , {0.0f, 0.0f, 4.0f}
+        , {0.0f, 0.0f, 9.0f}
+    };
+    std::vector<mesh::Node<float>> actual   = engine.normal(mesh, "./shaders/tomos.kernel");
+
+    ASSERT_EQ(actual.size(), expected.size());
+    for (std::size_t i = 0; i < actual.size(); i++) { EXPECT_EQ(actual[i], expected[i]); }
+}
+
+
 TEST(Stiffness, Oracle) {
     const mesh::Mesh<float> mesh = {
         {2.2, 0, 8}
