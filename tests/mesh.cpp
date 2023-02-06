c2fc73e11cd23650cfab494dbf45df3b456af6f2
Pedro Henrique Dalla Pacce <pedrodallapacce@gmail.com>
2023-02-06 11:15:15 -0300

    feat: adds element handling

--- tests/mesh.cpp
+++ tests/mesh.cpp
@@ -3,7 +3,7 @@
 #include <mesh/mesh.hpp>
 #include <tomos/tomos.hpp>
 
-TEST(Nodes, Lines) {
+TEST(Nodes, Parse) {
     std::string ss =
         "$Nodes\n"
         "6\n"
@@ -36,6 +36,32 @@ TEST(Nodes, Lines) {
     for (std::size_t j = 0; j < 3; j++) {
         EXPECT_EQ(actual[i].s[j], expected[i].s[j]);
     }
+    }
+}
+
+TEST(Elements, Parse) {
+    std::string ss = 
+        "$Elements\n"
+        "2\n"
+        "1 3 2 99 2 1 2 3 4\n"
+        "2 3 2 99 2 2 5 6 3\n"
+        "$EndElements\n"
+        ;
+    tomos::mesh::Elements expected = {
+          {tomos::mesh::element::Type::QUADRANGLE4, {0, 1, 2, 3}}
+        , {tomos::mesh::element::Type::QUADRANGLE4, {1, 4, 5, 2}}
+    };
+    tomos::mesh::Elements actual;
+
+    using Iterator = std::string::iterator;
+
+    tomos::mesh::decoder::elements<Iterator> grammar;
+    bool result = boost::spirit::qi::parse(ss.begin(), ss.end(), grammar, actual);
+
+    ASSERT_TRUE(result);
+    ASSERT_EQ(actual.size(), expected.size());
+    for (std::size_t i = 0; i < actual.size(); i++) {
+        EXPECT_EQ(actual[i], expected[i]);
     }
 }
 
