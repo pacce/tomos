49817dbe7e03b8e9ebeebe679a8782e69e0d6c37
Pedro Henrique Dalla Pacce <pedrodallapacce@gmail.com>
2023-02-13 23:48:44 -0300

    refactor: tests metis partition

--- tests/metis.cpp
+++ tests/metis.cpp
@@ -115,50 +115,50 @@ TEST(Dual, Edge) {
 //         }
 //     }
 // }
-// 
-// TEST(Partition, Edge2) {
-//     tomos::metis::Dual dual(MESH, tomos::metis::Common::EDGE);
-//     tomos::metis::Partitions actual     = dual.partition(2);
-//     tomos::metis::Partitions expected   = {
-//           {1, 0}
-//         , {2, 1}
-//         , {3, 1}
-//         , {4, 1}
-//         , {5, 0}
-//         , {6, 0}
-//         , {7, 0}
-//         , {8, 1}
-//     };
-// 
-//     ASSERT_EQ(actual.size(), expected.size());
-// 
-//     for (auto& [k, x] : expected) {
-//         tomos::metis::Partition& y = actual[k];
-//         EXPECT_EQ(x, y);
-//     }
-// }
-// 
-// TEST(Partition, Edge4) {
-//     tomos::metis::Dual dual(MESH, tomos::metis::Common::EDGE);
-//     tomos::metis::Partitions actual     = dual.partition(4);
-//     tomos::metis::Partitions expected   = {
-//           {1, 1}
-//         , {2, 3}
-//         , {3, 3}
-//         , {4, 2}
-//         , {5, 1}
-//         , {6, 0}
-//         , {7, 0}
-//         , {8, 2}
-//     };
-// 
-//     ASSERT_EQ(actual.size(), expected.size());
-// 
-//     for (auto& [k, x] : expected) {
-//         tomos::metis::Partition& y = actual[k];
-//         EXPECT_EQ(x, y);
-//     }
-// }
+
+TEST(Partition, Edge2) {
+    tomos::metis::Dual dual(MESH, tomos::metis::Common::EDGE);
+    tomos::metis::Partitions actual     = dual.partition(2);
+    tomos::metis::Partitions expected   = {
+          {0, 0}
+        , {1, 1}
+        , {2, 1}
+        , {3, 1}
+        , {4, 0}
+        , {5, 0}
+        , {6, 0}
+        , {7, 1}
+    };
+
+    ASSERT_EQ(actual.size(), expected.size());
+
+    for (auto& [k, x] : expected) {
+        tomos::metis::Partition& y = actual[k];
+        EXPECT_EQ(x, y);
+    }
+}
+
+TEST(Partition, Edge4) {
+    tomos::metis::Dual dual(MESH, tomos::metis::Common::EDGE);
+    tomos::metis::Partitions actual     = dual.partition(4);
+    tomos::metis::Partitions expected   = {
+          {0, 1}
+        , {1, 3}
+        , {2, 3}
+        , {3, 2}
+        , {4, 1}
+        , {5, 0}
+        , {6, 0}
+        , {7, 2}
+    };
+
+    ASSERT_EQ(actual.size(), expected.size());
+
+    for (auto& [k, x] : expected) {
+        tomos::metis::Partition& y = actual[k];
+        EXPECT_EQ(x, y);
+    }
+}
 
 int
 main(int argc, char** argv) {
